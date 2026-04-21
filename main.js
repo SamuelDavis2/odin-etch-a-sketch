ed520e3953120c21cc42876a8da4703b73ae630e
SamuelDavis2 <samueldavis531@gmail.com>
2026-04-21 16:49:41 +1000

    recreating better grid layout

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const gridSize = document.querySelector(".gridSize");
 const button = document.querySelector("button");
 
 function generateDivs(num) {
-    let basis = 100 / Math.sqrt(num);
-    for (let i = 0; i < num; i++) {
+    let basis = 100 / num;
+    for (let i = 0; i < num * num; i++) {
         const div = document.createElement("div");
         div.classList.add("grid");
         container.appendChild(div);
@@ -15,4 +15,20 @@ function generateDivs(num) {
     }
 }
 
+const grid = document.querySelectorAll(".grid");
+
+function resetGrid() {
+    grid.forEach(cell => {
+        cell.style.backgroundColor = "white";
+    });
+}
+
+function removeGrid() {
+    grid.forEach(cell => {
+        cell.remove();
+    });
+}
+
+button.addEventListener("click", removeGrid);
 button.addEventListener("click", () => generateDivs(Number(gridSize.value)));
+button.addEventListener("click", resetGrid);
